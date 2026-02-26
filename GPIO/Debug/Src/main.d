8e001626ca1db5d93b2734f6aeaae69d8f2abb40
Jose-Figueroa <jfigueroa@rootstack.com>
2026-02-26 15:56:29 -0600

    first commit

---
+++ GPIO/Debug/Src/main.d
@@ -0,0 +1,17 @@
+Src/main.o: ../Src/main.c \
+ /home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Device/ST/STM32F4xx/Include/stm32f401xe.h \
+ /home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/core_cm4.h \
+ /home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/cmsis_version.h \
+ /home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/cmsis_compiler.h \
+ /home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/cmsis_gcc.h \
+ /home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/mpu_armv7.h \
+ /home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Device/ST/STM32F4xx/Include/system_stm32f4xx.h
+/home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+/home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Device/ST/STM32F4xx/Include/stm32f401xe.h:
+/home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/core_cm4.h:
+/home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/cmsis_version.h:
+/home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/cmsis_compiler.h:
+/home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/cmsis_gcc.h:
+/home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Include/mpu_armv7.h:
+/home/rootstack/Documents/Projects/Arnott/Firmware-Codes/Drivers/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
